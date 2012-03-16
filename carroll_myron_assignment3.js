2bff8fded26f21cd33cd318d4353bf0318fb019c
Myron Carroll <myronbennell@fullsail.edu>
2012-03-15 22:33:10 -0400

    Done with the meat
    Mostly done with code. Now to clean things up and finish the project.

--- carroll_myron_assignment3.js
+++ carroll_myron_assignment3.js
@@ -6,7 +6,8 @@ var say = function (message){
 
 var budget = 2000,
 	studio = 500,
-	dogwood = true
+	getStudio = false,
+	doCover = true;
 
 say("I have finished recording the new single for a new artist.");
 say("Now I have to mix and master the record.");
@@ -26,28 +27,58 @@ payInvoice("Steve", 150, "he is not a well know player. But he is a beast");
 payInvoice("Quinton",  200, "he has a pretty legnthy resume");
 payInvoice("Leeland", 100, "he is new to recording but did a great job");
 payInvoice("Ronald", 250, "he has several albums out and can lend some brand recognition to buyers");
-say("I already paid the recording studio $" + studio + " at the end of the recording session.");
 
 say("Now I must mix the record. I can do this myself. But I need to book more studio time.");
 say("I figure that I can get a really good mix on the record in 4 hours. And I need to do this today.");
 say("So I need to find a studio that is available today.");
 say("I have a different studio in mind for this. It is one that is near my house and I have heard some really good mixes come out of there.");
 
-if (dogwood = true) {
+
+
+if (getStudio = true){
 	say("I called Dogwood Studios and they have a slot available today");
-} 
-else {
-	say("I couldn't get the studio I wanted, so I have to call around to some other spots.")
+}else{
+	console.log("I couldn't get the studio I wanted, so I have to get some suggestions for places to mix.")
 	var studios = ["Doppler", "Patchwork", "Crossover"];
   studioavail = ["not available", "available", "available"];
 for (var i=0, a=studios.length; i<a ; i++ ){
-	say("I called " +studios[i]+ " Studios and they said that they are " +studioavail[i]+ " in that time frame")
+	console.log("I called " +studios[i]+ " Studios and they said that they are " +studioavail[i]+ " in that time frame")
 };
 
 };
 
+say("Now I am in the mix session. I have to get this record done quickly because I only have 4 hours to mix.")
+say("I have a total of 9 different parts to mix. They each have varying amounts of complexity. But I need to tackle them one by one.")
+
+var trackNamesbyInstrument = ["drums", "keys", "bass", "saxophone", "Background Vox1", "Background Vox2", "Background Vox3", "Lead Vox"], 
+	wholeMinutesPerTrack = [60, 20, 15, 20, 15, 15, 15, 35];
+var mixOneInstrument = function(trackName, minutesThisTrack) {
+	
+	console.log("Starting to mix the " + trackName + " for " + minutesThisTrack + " minutes");
+	for (var minutes = 0; minutes < minutesThisTrack; minutes += 5) {
+		var minutesRemain = minutesThisTrack - minutes;
+		console.log(minutes + " done, " + minutesRemain + " to go!");
+	
+	};
+	console.log("I'm done with the " + trackName + ".");
+};
+var mixAllTracks = function(trackNames, minutesPerTrack){
+	for (var trackNumber = 0; trackNumber < trackNames.length; trackNumber++) {
+		var trackName = trackNames[trackNumber],
+		minutesThisTrack = minutesPerTrack[trackNumber];
+	mixOneInstrument(trackName, minutesThisTrack);
+	};
 
 
+};
+mixAllTracks(trackNamesbyInstrument, wholeMinutesPerTrack);
+say("I will send this off to be mastered this tonight. this will take another day to complete.");
+say("While that's being done, I must get the credits together to put in the cover art and decide if I even want to do the cover art.");
+if (doCover = false){
+	say("I'm kinda tired. After all this work, I can spend the money to have a friend go the cover art. It's no big deal");
+}else{
+	say("I am feeling kinda cheap. If I go ahead and do the cover art, I can keep this money. I'll sleep when I die")
+};
 
 
 
@@ -59,3 +90,10 @@ var handleData = function (json) {
 };
 
 handleData(json2);
+
+var javail=false
+if (javail=true){
+	console.log("Jason is available, I don't have to sing the male parts. I am really out of practice")
+	}else{
+	console.log("Oh well, Jason is not available. I have to man up and sing these parts. That's why they pay me the big bucks.")	
+};
